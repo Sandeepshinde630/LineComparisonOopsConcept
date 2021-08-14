865812fcccc5c251cb65951a131cfb6d3e688a2f
unknown <sandeepshinde630@gmail.com>
2021-08-14 13:14:37 +0530

    feat:Finding Length of two Lines

--- LineComparisonDay8/Program.cs
+++ LineComparisonDay8/Program.cs
@@ -7,6 +7,13 @@ namespace LineComparisonDay8
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to LineComparison Problem");
+            LineCompare obj = new LineCompare(5, 9, 10, 21);
+            double LineOne = obj.Length();
+            Console.WriteLine("Length of Line1:" + LineOne);
+
+            LineCompare obj1 = new LineCompare(1, 3, 1, 3);
+            double LineTwo = obj1.Length();
+            Console.WriteLine("Length of Line2:" + LineTwo);
         }
     }
 }
